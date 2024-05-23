c5015d85b47c20e934a1394270fd0a9b6cf75dea
Ivan Tan <ivantanyenwen02@gmail.com>
2024-05-23 18:49:42 +0800

    remove unnecessary files

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,11 @@
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideRouter } from '@angular/router';
 import { AppRoutingModule } from './app/app-routing.module';
-import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserModule, AppRoutingModule),provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideHttpClient()]
+  providers: [importProvidersFrom(BrowserModule, AppRoutingModule),provideZoneChangeDetection({ eventCoalescing: true }), provideAnimationsAsync(), provideHttpClient()]
 })
   .catch((err) => console.error(err));
