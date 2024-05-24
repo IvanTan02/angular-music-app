00554012aad4c711966f8f0c11f8877859fc4946
Ivan Tan <ivantanyenwen02@gmail.com>
2024-05-24 08:32:57 +0800

    add profile component

---
+++ frontend/src/app/modules/profile/profile.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import SpotifyWebApi from 'spotify-web-api-node';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.scss'
+})
+export class ProfileComponent {
+
+}
